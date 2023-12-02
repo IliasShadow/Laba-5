55fbf62aad6fc4238822565ebcf1724ff0701a02
IliasShadow <145995986+IliasShadow@users.noreply.github.com>
2023-12-02 23:07:14 +1000

    Добавлен дочерний класс Student

--- Multy.cpp
+++ Multy.cpp
@@ -8,8 +8,13 @@ protected:
 public:
     Human(){
         name="Nobody";
-        age=18;
-        height=180;
+        age=15;
+        height=170;
+    }
+    Human(string s1,int s2,int s3){
+        name=s1;
+        age=s2;
+        height=s3;
     }
     void setName(string s){
         name=s;
@@ -30,6 +35,23 @@ public:
         return height;
     }
 
+};
+class Student:protected Human{
+protected:
+    string school;
+public:
+    Student(){
+        name="Nobody";
+        age=18;
+        height=180;
+        school="No school"
+    }
+    Student(string s1,int s2,int s3,string s4{
+        name=s1;
+        age=s2;
+        height=s3;
+        school=s4;
+    }
 };
 int main(){
     Human Me,You,He;
