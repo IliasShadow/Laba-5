8e153c4d6388b4b806bd4cfc96c9ea9de0e97e7e
IliasShadow <145995986+IliasShadow@users.noreply.github.com>
2023-12-02 22:52:51 +1000

    Добавлены геттеры и сеттеры для полей класса Human

--- Multy.cpp
+++ Multy.cpp
@@ -11,9 +11,29 @@ public:
         age=18;
         height=180;
     }
+    void setName(string s){
+        name=s;
+    }
+    string getName(){
+        return name;
+    }
+    void setAge(int s){
+        Age=s;
+    }
+    string getAge(){
+        return age;
+    }
+    void setHeight(int s){
+        height=s;
+    }
+    string getHeight(){
+        return height;
+    }
+
 };
 int main(){
     Human Me,You,He;
     setlocale(0,"");
     return 0;
 }
+
